3dbb7a87c160a22fece4c0f9faf4868712e0ee29
olrand <olrand.dev@gmail.com>
2020-01-14 12:57:53 +0200

    show entries list, pagination

--- blog/apps/base/views.py
+++ blog/apps/base/views.py
@@ -1,10 +1,22 @@
+from itertools import chain
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import JsonResponse
 from blog.utils import ApiResponse
+from blog import const
 from .models import Entry, Author, Article, VideoArticle
 
 def index(request):
-    return render(request, 'base/index.html')
+    entries_list = get_entries_list(request)
+
+    per_page = request.GET.get('per_page', const.PER_PAGE)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(entries_list, per_page)
+    page_obj = paginator.get_page(page)
+
+    return render(request, 'base/index.html', {
+        'page_obj': page_obj
+    })
 
 
 def show_entry_page(request):
@@ -57,26 +69,24 @@ def get_user_data(request):
 
 
 def get_entries_list(request):
-    resp = ApiResponse()
+
     entries = Entry.objects.all()
 
-    articles = list(Article.objects.all().values())
-    video_articles = list(VideoArticle.objects.all().values())
-    entries_data = articles + video_articles
-    entries_data = sorted(entries_data, key=lambda i: i['pub_date'], reverse=True)
+    articles = Article.objects.all()
+    video_articles = VideoArticle.objects.all()
+    entries_data = sorted(chain(articles, video_articles), key=lambda i: i.pub_date)
 
     for entry_data in entries_data:
-        entry = entries.get(pk=entry_data['id'])
-        author = Author.objects.get(pk=entry_data['author_id'])
+        entry = entries.get(pk=entry_data.id)
+        author = Author.objects.get(pk=entry_data.author_id)
 
-        entry_data['category'] = entry.category.name
-        entry_data['pub_date'] = entry.pub_date.strftime("%b %d, %Y")
-        entry_data['author'] = f'{author.user.first_name} {author.user.last_name}'
-        entry_data['excerpt'] = entry_data['text'][:150]
-        entry_data['image_thumb_link'] = entry.image_thumb.url
-        entry_data['entry_page_link'] = f"articles/{entry_data['entry_type']}/{entry_data['id']}/"
-        entry_data['cat_list_link'] = f"category/{entry.category.alias}/"
-        entry_data['month_archive_link'] = f"archive/{entry.pub_date.year}/{entry.pub_date.month}/"
+        entry_data.category_name = entry.category.name
+        entry_data.pub_date_formated = entry.pub_date.strftime("%b %d, %Y")
+        entry_data.author_name = f'{author.user.first_name} {author.user.last_name}'
+        entry_data.excerpt = entry_data.text[:150]
+        entry_data.image_thumb_link = entry.image_thumb.url
+        entry_data.entry_page_link = f"articles/{entry_data.entry_type}/{entry_data.id}/"
+        entry_data.cat_list_link = f"category/{entry.category.alias}/"
+        entry_data.month_archive_link = f"archive/{entry.pub_date.year}/{entry.pub_date.month}/"
 
-    resp.data = entries_data
-    return JsonResponse(resp.get_resp_data())
+    return entries_data
